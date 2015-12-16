fff763bca59792e39c2d4013897a2807fe83163f
Zentenzah <IzumiPMA@gmail.com>
2015-12-16 21:51:11 +0100

    Summarizer stump.

---
+++ Summarization Scala/src/main/scala/graph/spark/summarization/GraphSummarizer.scala
@@ -0,0 +1,11 @@
+package graph.spark.summarization
+
+import org.apache.spark.graphx.Graph
+
+object GraphSummarizer
+{
+    def summarize[VD, ED]( graph: Graph[VD, ED] ): Graph[VD, ED] =
+    {
+        null
+    }
+}
